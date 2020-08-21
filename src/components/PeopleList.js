3fd513d2ddf91b09e425ac2e613adc12491ee351
John Shearer <john@ultranaut.com>
2020-08-21 15:54:45 -0400

    Wire up the PeopleList component

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectPerson } from '../actions';
 
 class PeopleList extends Component {
+  renderList() {
+    return this.props.people.map((person, idx) => {
+      return (
+        <div className="item" key={idx}>
+          <div className="right floated content">
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectPerson(person)}
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">{person.first}</div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>PeopleList</div>;
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
-export default PeopleList;
+
+const mapStateToProps = (state) => {
+  return { people: state.people };
+};
+
+export default connect(mapStateToProps, { selectPerson })(PeopleList);
